ba282a0dea5f0b35f82b1e1cde8d04c67a386b79
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-09-18 16:14:16 +0300

    add task9.3

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите значение N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(n, 1));
+
+
+int PrintNumbers(int n, int l)
+{
+    if (n == l) return n;
+    else Console.Write($"{PrintNumbers(n, l + 1)}, ");
+    return l;
+}
+ 
